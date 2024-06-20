aa3396f5c8e17d49f21294adc8e03f02b8cd66c7
gonids4545 <136231250+gonids4545@users.noreply.github.com>
2024-06-20 16:04:05 +0530

    Added comments

--- src/actions/Actions.js
+++ src/actions/Actions.js
@@ -1,10 +1,11 @@
+//this is like Java's Enum. Fixed set of options
 const ACTIONS = {
-    JOIN: 'join',
-    JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
-    CODE_CHANGE: 'code-change',
-    SYNC_CODE: 'sync-code',
-    LEAVE: 'leave',
+  JOIN: "join",
+  JOINED: "joined",
+  DISCONNECTED: "disconnected",
+  CODE_CHANGE: "code-change",
+  SYNC_CODE: "sync-code",
+  LEAVE: "leave",
 };
 
 module.exports = ACTIONS;
